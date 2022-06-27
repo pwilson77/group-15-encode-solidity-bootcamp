4a4d29609cb352c99f64d2cb454af791db2c8c9a
saumyat99 <qoolsom@outlook.com>
2022-06-27 19:56:53 +0530

    giveVotingRights

--- scripts/Ballot/queryingProposals.ts
+++ scripts/Ballot/queryingProposals.ts
@@ -2,10 +2,13 @@ import { Contract, ethers } from "ethers";
 import "dotenv/config";
 import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json";
 import { Ballot } from "../../typechain";
-import { getInfuraProvider, getWallet } from "../../lib/config";
 
 async function main() {
-  const provider = getInfuraProvider("ropsten"), wallet = getWallet(),signer = wallet.connect(provider);
+  const wallet =
+    process.env.MNEMONIC && process.env.MNEMONIC.length > 0
+      ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
+      : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+  const provider = ethers.providers.getDefaultProvider("ropsten"),signer = wallet.connect(provider);
   console.log(`Using address ${wallet.address}`);
   if (process.argv.length < 3) throw new Error("Address of ballot is missing");
   const ballotAddress = process.argv[2];
